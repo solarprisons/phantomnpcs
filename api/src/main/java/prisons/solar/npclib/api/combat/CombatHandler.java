18154e5e042a1f25244ec0ec8b166180b7743a52
connorwitmer <connorwitmer34@gmail.com>
2025-12-19 19:55:51 -0500

    feat(api): add CombatHandler interface (WIP)
    Handler interface for combat logic using Combatant types,
    supporting both NPCs and Players through common interface.

--- api/src/main/java/prisons/solar/npclib/api/combat/CombatHandler.java
+++ api/src/main/java/prisons/solar/npclib/api/combat/CombatHandler.java
@@ -1,4 +1,10 @@
-package prisons.solar.npclib.core.combat;
+package prisons.solar.npclib.api.combat;
+
+import prisons.solar.npclib.api.npc.NPC;
 
 public interface CombatHandler {
+    CombatComponent.AttackResult handleAttack(Combatant attacker, Combatant target);
+    boolean canAttack(Combatant attacker, Combatant target);
+    void onCombatStart(Combatant npc, Combatant target);
+    void onCombatEnd(Combatant npc);
 }
