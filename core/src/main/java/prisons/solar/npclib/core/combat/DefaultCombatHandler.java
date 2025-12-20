a21b64d64d64cf2ad9fa068ca72fe2bc66693483
connorwitmer <connorwitmer34@gmail.com>
2025-12-19 19:56:02 -0500

    feat(core): add DefaultCombatHandler implementation (WIP)
    Default implementation of CombatHandler interface.

--- core/src/main/java/prisons/solar/npclib/core/combat/DefaultCombatHandler.java
+++ core/src/main/java/prisons/solar/npclib/core/combat/DefaultCombatHandler.java
@@ -1,4 +1,27 @@
 package prisons.solar.npclib.core.combat;
 
-public class DefaultCombatHandler {
+import prisons.solar.npclib.api.combat.CombatComponent;
+import prisons.solar.npclib.api.combat.CombatHandler;
+import prisons.solar.npclib.api.combat.Combatant;
+
+public class DefaultCombatHandler implements CombatHandler {
+    @Override
+    public CombatComponent.AttackResult handleAttack(Combatant attacker, Combatant target) {
+        return null;
+    }
+
+    @Override
+    public boolean canAttack(Combatant attacker, Combatant target) {
+        return false;
+    }
+
+    @Override
+    public void onCombatStart(Combatant npc, Combatant target) {
+
+    }
+
+    @Override
+    public void onCombatEnd(Combatant npc) {
+
+    }
 }
